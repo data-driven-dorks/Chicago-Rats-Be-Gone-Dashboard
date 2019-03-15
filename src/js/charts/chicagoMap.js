158866342c93990c65f54dbcfc9a5ddb2aad6aba
heguanelvis <heguanelvis@gmail.com>
2019-03-15 00:01:57 -0500

    added basic fill scale to map and going to add slider control soon

--- src/js/charts/chicagoMap.js
+++ src/js/charts/chicagoMap.js
@@ -39,20 +39,38 @@ export default class chicagoMap {
         this.geoGenerator = d3.geoPath(this.projection);
     };
 
+    graphFillScale() {
+        this.fill = d3.scaleOrdinal()
+            .domain([0, 754])
+            .range([
+                "#fcee21", "#fceb22", "#fce722", "#fce522", "#fce223", 
+                "#fbdf23", "#fbdb23", "#fbd924", "#fbd624", "#fbd324", 
+                "#facf25", "#facd25", "#faca25", "#fac625", "#f9c326", 
+                "#f9c026", "#f9bd26", "#f8ba26", "#f8b626", "#f8b327", 
+                "#f7b027", "#f7ad27", "#f6aa27", "#f6a627", "#f5a427", 
+                "#f5a127", "#f49e27", "#f49a27", "#f39828", "#f39328", 
+                "#f29028", "#f28d28", "#f18928", "#f08628", "#f08428", 
+                "#ef8028", "#ef7c28", "#ee7928", "#ed7528", "#ed7128", 
+                "#ec6f28", "#eb6b28", "#ea6728", "#ea6328", "#e95f27", 
+                "#e85c27", "#e85827", "#e75427", "#e65027", "#e54b27"
+            ])
+    };
+
     graphMap() {
         this.graph.selectAll("path")
             .data(this.dataChicago.features)
             .enter()
             .append("path")
             .attr("d", this.geoGenerator)
-            .style("stroke", "white")
+            .style("stroke", "#252525")
             .style("stroke-width", 2)
-            .attr("fill", "rgba(0, 0, 0, 0)");
+            .attr("fill", d => this.fill(d.properties.data[0].complaintspc));
     };
 
     grapher() {
         this.graphSetup();
         this.graphProjection();
+        this.graphFillScale();
         this.graphMap();
         this.graphed = true;
     }
